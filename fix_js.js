e5351889c2cef6315980642469bc857350f3c747
Dafa Hentra <63453455+dafahentra@users.noreply.github.com>
2026-06-06 01:58:16 +0700

    update audit

--- fix_js.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const file = '/Users/user/Documents/GitHub/Works-portfolio/src/assets/js/components/work-slider.js';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace("const containerEl = document.querySelector('#recent-work').previousElementSibling.querySelector('.container');", "const containerEl = document.querySelector('#recent-work').previousElementSibling;");
-
-fs.writeFileSync(file, content);
